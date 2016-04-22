079093950fc6b3ef1606adf6d43bd66b2047cfd4
Jaken Herman <jch053@shsu.edu>
2016-04-22 12:34:00 -0500

    Update stat_final.R

--- stat_final.R
+++ stat_final.R
@@ -1,11 +1,12 @@
-#setwd("C:/Users/Jaken/Desktop")
-
+library(RCurl)
+x <- getURL("https://raw.githubusercontent.com/JakenHerman/stat_final/master/Brain.csv")
 
 #####                 ##### 
 ##### Tumor Size Data #####
 #####                 #####
 
-tumor_size = read.csv("Brain.csv", colClasses=c("NULL", "NULL", NA, "NULL", "NULL", "NULL"))
+
+tumor_size <- read.csv(text = x, colClasses=c("NULL", "NULL", NA))
 tumor_size_omit_na <- na.omit(tumor_size)
 
 print("MEAN TUMOR SIZE: ")
@@ -20,7 +21,7 @@ sapply(tumor_size_omit_na, sd, na.rm = TRUE)
 ##### Age @ Diagnosis Data #####
 #####                      #####
 
-age_diagnosis = read.csv("Brain.csv", colClass=c("NULL", NA, "NULL", "NULL", "NULL", "NULL"))
+age_diagnosis = read.csv(text = x, colClass=c("NULL", NA, "NULL"))
 age_diagnosis_omit_na <- na.omit(age_diagnosis)
 
 print("MEAN AGE AT DIAGNOSIS: ")
@@ -30,6 +31,8 @@ sapply(age_diagnosis_omit_na, var, na.rm = TRUE)
 print("STANDARD DEVIATION OF AGE AT DIAGNOSIS: ")
 sapply(age_diagnosis_omit_na, sd, na.rm = TRUE)
 
+
 tumor_size_omit_na <- as.numeric(unlist(tumor_size_omit_na))
-hist(tumor_size_omit_na)
+tumor_size_omit_na <- as.numeric(unlist(tumor_size_omit_na))
+hist(tumor_dat)
 boxplot(tumor_size_omit_na)
